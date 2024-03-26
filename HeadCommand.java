381802fc469829bcda27e3bc1d9274133039631f
nwSeiya-Arai <se-arai@networld.ne.jp>
2024-03-26 10:23:57 +0900

    コードの修正。読み込ませるテキストの修正。

--- HeadCommand.java
+++ HeadCommand.java
@@ -1,37 +1,52 @@
+import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 public class HeadCommand{
 	public static void main(String[] args){
-		//コマンド実行の引数を受取り、正しい引数が受取れているか確認する。
-		//正しい引数を受取れない場合、正しいコマンドの正解を表示しプログラムを終了する。
-		if (args.length != 2) {
-			System.err.println("Usage: java HeadCommand <file> <lines>");
-			//異常終了
+		if (args.length != 2 || (!args[1].startsWith("-c") && !args[1].startsWith("-n"))) {
+			System.err.println("Usage: java HeadCommand <file> <-c<bytes>> or <-n<lines>>");
 			System.exit(1);
 		}
 		
-		//ファイルパスを変数に格納する。
-		String filePath = args[0];
 		
-		//読み込むファイルのパスがあっているか確認する。
-		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-			//表示する行の数を変数に格納する。
-		    int linesToShow = Integer.parseInt(args[1]);
-			//読み込んだ行の内容を格納するための変数
+		String filePath = args[0];
+		boolean optionC = args[1].startsWith("-c");
+		int num = Integer.parseInt(args[1].substring(2));
+		
+		
+		try {
+            if (optionC) {
+                readByBytes(filePath, num);
+            } else {
+                readByLines(filePath, num);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+	}
+		
+		private static void readByBytes(String filePath, int numBytes)throws IOException{
+			FileInputStream inputStream = new FileInputStream(filePath);
+			 byte[] buffer = new byte[numBytes];
+            int bytesRead = inputStream.read(buffer);
+            if (bytesRead != -1) {
+                String content = new String(buffer, 0, bytesRead);
+                System.out.println(content);
+            }
+		}
+		
+		private static void readByLines(String filePath, int numLines)throws IOException{
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
 			String line;
-			//表示した行数をカウントするための変数
 			int linesRead = 0;
-			//ファイルの中身を1行ずつ読み込み、指定された行数までの内容を表示する。
-			while ((line =reader.readLine()) != null && linesRead < linesToShow) {
+			while ((line =reader.readLine()) != null && linesRead < numLines) {
 				System.out.println(line);
 				linesRead++;
 			}
-		} catch (IOException | NumberFormatException e) {
-			//読み込むファイルに問題がある場合エラーメッセージを表示する
-			System.err.println(e.getMessage());
 		}
-	}
+			
+	
 }
 
 
